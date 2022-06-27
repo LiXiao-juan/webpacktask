6a97717d0072bb19833d8ac5775a88817fd187d5
Li <1627843848@qq.com>
2022-06-27 20:13:17 +0800

    完成了清除dist目录 mode警告

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path")
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // 引入自动生成 html 的插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: "./src/main.js", // 入口
     output: { 
         path: path.join(__dirname, "dist"), // 出口路径
@@ -12,6 +15,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: 'index.html'
-          })
-    ]
+          }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        port: 3000, // 端口号
+        open: true
+      }
 }
